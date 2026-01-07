6979cf41b6f34c6c237091dab9bde4b0d354435f
KennBalino <116750260+Kleinnnn1@users.noreply.github.com>
2026-01-07 12:34:19 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landpage from './pages/landpage/Landpage';
-import BrowsePage from './pages/browsepage/BrowsePage';
-import RecruiterPage from './pages/profilepage/RecruiterPage';
-import DeveloperPage from './pages/profilepage/DeveloperPage';
-import AdventurerPage from './pages/profilepage/AdventurerPage';
-import StalkerPage from './pages/profilepage/StalkerPage';
-import SkillsPage from './pages/boxpage/SkillsPage';
-import WorkExperiencePage from './pages/boxpage/WorkExperiencePage';
+
+import Landpage from "./pages/landpage/Landpage";
+import BrowsePage from "./pages/browsepage/BrowsePage";
+import RecruiterPage from "./pages/profilepage/RecruiterPage";
+import DeveloperPage from "./pages/profilepage/DeveloperPage";
+import AdventurerPage from "./pages/profilepage/AdventurerPage";
+import StalkerPage from "./pages/profilepage/StalkerPage";
+
+import SkillsPage from "./pages/boxpage/SkillsPage";
+import WorkExperiencePage from "./pages/boxpage/WorkExperiencePage";
 import ProjectsPage from "./pages/boxpage/ProjectsPage";
 import ContactMePage from "./pages/boxpage/ContactMePage";
 import MusicPage from "./pages/boxpage/MusicPage";
@@ -15,13 +17,19 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Landpage />} /> */}
-        <Route path="/" element={<RecruiterPage />} />
+        {/* Default route */}
+        <Route path="/" element={<ProjectsPage />} />
+
+        {/* Other pages */}
         <Route path="browse" element={<BrowsePage />} />
+
+        {/* Profile pages */}
+        <Route path="profile/recruiter" element={<RecruiterPage />} />
         <Route path="profile/developer" element={<DeveloperPage />} />
         <Route path="profile/adventurer" element={<AdventurerPage />} />
         <Route path="profile/stalker" element={<StalkerPage />} />
-        <Route path="profile/recruiter" element={<RecruiterPage />} />
+
+        {/* Box pages */}
         <Route path="skills" element={<SkillsPage />} />
         <Route path="work-experience" element={<WorkExperiencePage />} />
         <Route path="projects" element={<ProjectsPage />} />
